51a9dd70e2f410819680c98a2e23d0fb1a1dd604
Alexander Altman <qewhouse@gmail.com>
2023-02-22 20:14:03 +0300

    Initial Commit

---
+++ TimerApp/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  TimerApp
+//
+//  Created by Alexander Altman on 22.02.2023.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
